7c9fb08a0217e39b83dbf15160749edfd839c3d6
tricia <pcampbell.dawson@gmail.com>
2015-08-30 19:10:41 -0400

    delete unused menu code

--- src/ca/campbell/samplelinearlayout/MainActivity.java
+++ src/ca/campbell/samplelinearlayout/MainActivity.java
@@ -2,8 +2,6 @@ package ca.campbell.samplelinearlayout;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
